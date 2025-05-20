fd8932159a3817b48f77639e6f9ab3e9a84c44b2
nabab <thomas.nabet@gmail.com>
2025-05-20 04:13:48 +0200

    Fix

--- src/mvc/public/_super.php
+++ src/mvc/public/_super.php
@@ -1,6 +1,3 @@
 <?php
 /** @var bbn\Mvc\Controller $ctrl */
-if ( !\defined('APPUI_NOTE_ROOT') ){
-  define('APPUI_NOTE_ROOT', $ctrl->pluginUrl('appui-bookmark').'/');
-}
-$ctrl->data['root'] = $ctrl->pluginUrl('appui-bookmark').'/';
+$ctrl->addData(['root' => $ctrl->pluginUrl('appui-bookmark').'/']);
